bbf401e73ba2cda6f9400aa9b3635931513a5bbb
fernando-castelo <nando.castelobranco24@hotmail.com>
2023-12-15 18:35:57 -0300

    First Commit

---
+++ src/test/java/com/tusk/todolistbackend/TodolistBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tusk.todolistbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
